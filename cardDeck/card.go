79ba4ba67ec1ce328fa461e1c8826d643ccf3889
aThorp96 <ahthorp96@icloud.com>
2017-11-10 16:23:45 -0500

    Began fleshing out the card struct.

--- cardDeck/card.go
+++ cardDeck/card.go
@@ -6,9 +6,25 @@ package cardDeck
 * Suit must be "HEARTS", "CLUBS", "DIAMONDS", or "SPADES".
 * Rank is a number 2-14, with 11-13 being face cards and 14 being Ace. 
 */
-type card struct{
-    Suit int16
-    Rank int64
+type Card struct{
+    suit int64
+    rank int64
 }
+
+// Suit returns the suit of the card.
+func (card *Card) Suit() int64 {
+    return card.suit
+}
+
+//Rank returns the rank of the card.
+func (card *Card) Rank() int64 {
+    return card.rank
+}
+
+//New returns a new Card made from a passed in rank and suit (in that order).
+func New(rank int64, suit int64) *Card {
+    retrun &Card{suit, rank}
+}
+
 
 
