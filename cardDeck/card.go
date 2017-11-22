f5a815801e87aad4ed6a3922bdc441548a4a1368
aThorp96 <ahthorp96@icloud.com>
2017-11-22 16:05:50 -0500

    Fized spelling error

--- cardDeck/card.go
+++ cardDeck/card.go
@@ -3,7 +3,7 @@ package cardDeck
 
 /*
 * card stuct is a single card and has a suit and value.
-* Suit must be "HEARTS", "CLUBS", "DIAMONDS", or "SPADES".
+* Suit is a number from 0:3. 0 = Hearts, 1 = Clubs, 2 = Diamonds, 3 = Spades.
 * Rank is a number 2-14, with 11-13 being face cards and 14 being Ace. 
 */
 type Card struct{
@@ -23,7 +23,7 @@ func (card *Card) Rank() int64 {
 
 //New returns a new Card made from a passed in rank and suit (in that order).
 func New(rank int64, suit int64) *Card {
-    retrun &Card{suit, rank}
+    return &Card{suit, rank}
 }
 
 
