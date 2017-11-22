63a4aff9761e1ea67ef178ebdc46999702997442
aThorp96 <ahthorp96@icloud.com>
2017-11-22 16:07:22 -0500

    Added necessary functions to the deck: HasNext, Sort, Shuffle, Deal, New.

--- cardDeck/deck.go
+++ cardDeck/deck.go
@@ -1,5 +1,56 @@
 package cardGame
 
+import "math/rand"
+
 type Deck struct {
     cards Card[52]
+    numCards int64
+}
+
+// NoCard will be returned if a card is attempted to be accessed, but there is no card.
+type NoCard struct {
+    Error string
+}
+
+//Creates a new deck in order. 
+func (deck *Deck) Sort() {
+    for i := 0; i < 4; i++ {
+        for j := 2; j < 15; j++ {
+            deck.cards[(i + 1) * j] := card.New(i, j)
+            deck.numCards++
+        }
+    }
+}
+
+//Creates, sorts, and returns a new deck. 
+func New() *Deck {
+    deck := &Deck{Card[52], 0}
+    deck.Sort()
+    return deck
+}
+
+//Randomizes the order of the deck. 
+func (deck *Deck) Shuffle() {
+    for i := 0; i < deck.numCards; i++ {
+        index = rand.Intn(52 - i) + i
+        temp := deck.cards[i]
+        deck.cards[i] = deck.cards[index]
+        deck.cards[index] = temp
+    }
+}
+
+//Deal pops the top card off the deck and returns it.
+//Returns an error if HasNext != true
+func (deck *Deck) Deal() (*Card, error) {
+    if !deck.HasNext() {
+        return nil, NoCard{"Deck is out of cards."}
+    }
+    deck.numCards--
+    return deck.cards[numCards], nil
+}
+
+//hasNext returns true if there are cards left to be dealt.
+func (deck *Deck) HasNext() bool {
+    return numCards > 0
+}
 
